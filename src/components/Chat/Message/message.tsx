52c3fa10dbf56d37acded428ff781ee1b0b71462
pengsy <songyang410@outlook.com>
2025-11-30 00:14:57 +0800

    opt: 聊天界面的信息对话布局完善以及信息滚动到最后的实现

--- src/components/Chat/Message/message.tsx
+++ src/components/Chat/Message/message.tsx
@@ -3,17 +3,50 @@
 import { Box } from '@mui/material';
 
 import styles from './message.module.scss';
+import { useEffect, useRef, useState } from 'react';
+
+const MeMessage = (props: { message: string }) => {
+    return (
+        <Box className={`${styles.messageItem} ${styles.myMessage}`}>
+            我的: {props.message}
+        </Box>
+    )
+}
+
+const OtherMessage = (props: { message: string }) => {
+    return (
+        <Box className={`${styles.messageItem} ${styles.otherMessage}`}>
+            对方的: {props.message}
+        </Box>
+    )
+}
 
 export const ChatMessage = () => {
-    const data = Array(150).fill(0);
+    const messageBoxRef = useRef<HTMLDivElement>(null);
+
+    const [messageList, setMessageList] = useState<number[]>([]);
+
+    useEffect(() => {
+        const data = Array.from({ length: 150 }, ((_, i: number) => i));
+        setMessageList(data);
+    }, [])
+
+    useEffect(() => {
+        if (messageBoxRef != null && messageBoxRef.current && messageList.length > 0) {
+            // 滚动到聊天界面最后一条
+            messageBoxRef.current.scrollTop = messageBoxRef.current.scrollHeight;
+        }
+    }, [messageList])
 
     return (
-        <Box className={styles.messageBox}>
+        <Box component={'div'} className={styles.messageBox} ref={messageBoxRef}>
             {
-                data.map((_, index: number) => {
-                    return (
-                        <div key={index}>message</div>
-                    );
+                messageList.map((v: number, index: number) => {
+                    if (v % 2 == 0) {
+                        return <OtherMessage key={index} message={`${v}`} />
+                    } else {
+                        return <MeMessage key={index} message={`${v}`} />
+                    }
                 })
             }
         </Box>
