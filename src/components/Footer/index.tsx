1ed2593b2e63f75c72c1132de0388bf4bba87bb8
pengsy <songyang410@outlook.com>
2025-12-16 13:35:59 +0800

    style: footer update

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,19 +1,22 @@
 
-const providerSx = {
-    color: 'hsla(0, 0%, 86.3%, 0.3)'
-};
-
 export default function Footer() {
     return (
-        <footer className={'w-full pt-0! pr-3! pb-9! pl-3! flex flex-col md:flex-row justify-center items-center gap-1.5 md:gap-3'}>
+        <footer className={'w-full pt-0! pr-3! pb-9! pl-3! flex flex-col justify-center items-center gap-3'}>
             <ProviderText txt={'某ICP备XXXXXXXXXX号-1'} />
             <ProviderText txt={'绿色聊天，遵循秩序，本站24H进行违规举报和内容审核'} />
         </footer>
     );
 }
 
-const ProviderText = (props: {txt: string}) => {
+const ProviderText = (props: { txt: string }) => {
     return (
-        <span className={'text-[0.5rem]'} style={providerSx}>{props.txt}</span>
+        <span
+            className={'text-[0.5rem]'}
+            style={{
+                color: 'hsla(0, 0%, 86.3%, 0.3)'
+            }}
+        >
+            {props.txt}
+        </span>
     );
 }
