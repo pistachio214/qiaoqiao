bae5ba5edeadc2ef0622fe0799655274b3cdf90a
pengsy <songyang410@outlook.com>
2025-12-17 13:51:02 +0800

    feth: 漂流瓶页面基础页面布局

--- src/app/(root)/bottle/page.tsx
+++ src/app/(root)/bottle/page.tsx
@@ -2,6 +2,13 @@
 
 export default function Bottle() {
     return (
-        <>bottle</>
+        <div className="text-white h-full md:w-4/5 w-full flex flex-col overflow-hidden p-4! border border-amber-200">
+            <div className="h-full w-full flex flex-col items-center">
+                <div className="relative flex flex-col justify-center items-center border border-blue-500" style={{flex: '1 1 0%'}}>
+                        探索
+                </div>
+                <div className="h-1/5 flex items-center text-sm border border-red-500"></div>
+            </div>
+        </div>
     );
 }
